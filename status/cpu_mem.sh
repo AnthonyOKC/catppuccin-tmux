5ae5e88ef2cdd2425e6467e264b6452f6f6eacea
AnthonyOKC <anthonyfloresokc@gmail.com>
2024-05-27 14:37:06 -0500

    Modify cpu mode and icon

--- status/cpu_mem.sh
+++ status/cpu_mem.sh
@@ -4,9 +4,9 @@ show_cpu_mem() {
   local index icon color text module
 
   index=$1
-  icon="$(get_tmux_option "@catppuccin_cpu_mem_icon" "")"
-  color="$(get_tmux_option "@catppuccin_cpu_mem_color" "$thm_blue")"
-  text="$(get_tmux_option "@catppuccin_cpu_mem__text" "#($TMUX_PLUGIN_MANAGER_PATH/tmux-mem-cpu-load/tmux-mem-cpu-load --cpu-mode 2 --averages-count 0 --interval 2)")"
+  icon="$(get_tmux_option "@catppuccin_cpu_mem_icon" "󰍛")"
+  color="$(get_tmux_option "@catppuccin_cpu_mem_color" "$thm_green")"
+  text="$(get_tmux_option "@catppuccin_cpu_mem__text" "#($TMUX_PLUGIN_MANAGER_PATH/tmux-mem-cpu-load/tmux-mem-cpu-load --cpu-mode 1 --averages-count 0 --interval 2)")"
 
   module=$(build_status_module "$index" "$icon" "$color" "$text")
 
